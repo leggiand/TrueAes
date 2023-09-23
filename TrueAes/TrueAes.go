bd802081c73e1870b498abd2f3b5f3598b8fc91e
leggiand <114882821+leggiand@users.noreply.github.com>
2023-09-23 20:17:43 +0200

    Update TrueAes.go
    Code optimization and fixing of some bugs

--- TrueAes/TrueAes.go
+++ TrueAes/TrueAes.go
@@ -29,6 +29,35 @@ func Decrypt(key string, mess string) string {
 	decrypted := string(decryptedByte)
 	return decrypted
 }
+func TotalUnpad(topad string) string {
+	var result = ""
+	if len(topad) < 16 {
+		tmp, _ := pkcs7pad.Unpad([]byte(topad))
+		result = string(tmp)
+		return result
+	} else if len(topad) == 16 {
+
+		tmp, _ := pkcs7pad.Unpad([]byte(topad))
+		result = string(tmp)
+	} else {
+		result = ""
+		buffer := make([]byte, 0)
+		for index, element := range []byte(topad) {
+			buffer = append(buffer, element)
+			if (index+1)%16 == 0 {
+				tmp, err := pkcs7pad.Unpad(buffer)
+				if err != nil {
+					result += string(buffer)
+				}
+				result += string(tmp)
+				buffer = nil
+			}
+		}
+		tmp, _ := pkcs7pad.Unpad(buffer)
+		result += string(tmp)
+	}
+	return result
+}
 
 // A recursive function to apply the Pkcs7padding to a string not multiple of 16
 func TotalPad(topad string) string {
@@ -41,15 +70,15 @@ func TotalPad(topad string) string {
 		return topad
 	} else {
 		result = ""
-		buffer := make([]int32, 0)
-		for index, element := range topad {
+		buffer := make([]byte, 0)
+		for index, element := range []byte(topad) {
 			buffer = append(buffer, element)
 			if (index+1)%16 == 0 {
-				result += TotalPad(string(buffer))
+				result += string(buffer)
 				buffer = nil
 			}
 		}
-		result += TotalPad(string(buffer))
+		result += string(pkcs7pad.Pad(buffer, 16))
 	}
 	return result
 }
@@ -57,8 +86,8 @@ func TotalPad(topad string) string {
 // A Simple Loop to encrypt strings larger then 16bytes
 func FullEncrypt(key string, todecr string) string {
 	var result = ""
-	buffer := make([]int32, 0)
-	for index, element := range todecr {
+	buffer := make([]byte, 0)
+	for index, element := range []byte(todecr) {
 		buffer = append(buffer, element)
 		if (index+1)%16 == 0 {
 			result += Encrypt(key, string(buffer))
@@ -71,8 +100,8 @@ func FullEncrypt(key string, todecr string) string {
 // A Simple Loop to decrypt strings larger then 16bytes
 func FullDecrypt(key string, todecr string) string {
 	var result = ""
-	buffer := make([]int32, 0)
-	for index, element := range todecr {
+	buffer := make([]byte, 0)
+	for index, element := range []byte(todecr) {
 		buffer = append(buffer, element)
 		if (index+1)%32 == 0 {
 			result += Decrypt(key, string(buffer))
